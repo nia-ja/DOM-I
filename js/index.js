0bf89d9286dde1133fa7aded7c9a17dd4500137e
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-01-29 20:41:59 -0800

    Finished navigation and main content

--- js/index.js
+++ js/index.js
@@ -38,5 +38,48 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+const navItem = document.querySelectorAll('a');
+console.log(navItem);
+navItem[0].textContent = 'Services';
+navItem[1].textContent = 'Product';
+navItem[2].textContent = 'Vision';
+navItem[3].textContent = 'Features';
+navItem[4].textContent = 'About';
+navItem[5].textContent = 'Contact';
+
+const logo = document.querySelector('#logo-img');
+logo.src = "img/logo.png";
+
+const ctaH1 = document.querySelector('.cta-text > h1');
+ctaH1.textContent = 'DOM Is Awesome';
+
+const ctaButton = document.querySelector('.cta-text > button');
+ctaButton.textContent = 'Get Started';
+
+const ctaImage = document.querySelector('#cta-img');
+ctaImage.setAttribute('src', 'img/header-img.png');
+
+const h4 = document.querySelectorAll('h4');
+h4[0].textContent = 'Features';
+h4[1].textContent = 'About';
+h4[2].textContent = 'Services';
+h4[3].textContent = 'Product';
+h4[4].textContent = 'Vision';
+h4[5].textContent = 'Contact';
+
+const p = document.querySelectorAll('p');
+p[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[3].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+p[4].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const middleImage = document.querySelector('#middle-img');
+middleImage.src = 'img/mid-page-accent.jpg';
+
+
+
